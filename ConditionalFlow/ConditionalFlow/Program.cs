80006f51c16c323aef107f67d024beebd3fbadeb
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-12-11 14:21:33 +0300

    Conditional flows explained

---
+++ ConditionalFlow/ConditionalFlow/Program.cs
@@ -0,0 +1,87 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+DateTime today = DateTime.Today;
+if (today.Month == 7 && today.Day == 1)
+{
+    Console.WriteLine("Türkay'ın doğum günü");
+}
+
+if (today.Month == 12 || today.Month == 2)
+{
+    Console.WriteLine("Kış");
+}
+else if (today.Month == 7 || today.Month == 8)
+{
+    Console.WriteLine("YAZ");
+}
+
+int number = 8;
+//ternary operatör:
+string message = number % 2 == 0 ? "Çift" : "Tek";
+//if (number % 2 == 0)
+//{
+//    message = "Çift";
+//}
+//else
+//{
+//    message = "Tektir";
+//}
+
+
+Console.WriteLine("Trafik Lambası seçin (K, S, Y)");
+string light = Console.ReadLine();
+
+if (light == "K")
+{
+    Console.WriteLine("DUR");
+}
+else if (light == "S")
+{
+    Console.WriteLine("Dikkat!");
+}
+else if (light == "Y")
+{
+    Console.WriteLine("GEÇ");
+}
+else
+{
+    Console.WriteLine("Hatalı giriş");
+}
+
+
+switch (light)
+{
+    case "K":
+        Console.WriteLine("DUR");
+        break;
+    case "S":
+        Console.WriteLine("Dikkat");
+        break;
+    case "Y":
+        Console.WriteLine("GEÇ");
+        break;
+    default:
+        Console.WriteLine("Hatalı giriş");
+        break;
+}
+
+switch (DateTime.Now.DayOfWeek)
+{
+    case DayOfWeek.Sunday:
+    case DayOfWeek.Saturday:
+        Console.WriteLine("Hafta sonu...");
+        break;
+    case DayOfWeek.Monday:
+    case DayOfWeek.Tuesday:
+    case DayOfWeek.Wednesday:
+    case DayOfWeek.Thursday:
+    case DayOfWeek.Friday:
+        Console.WriteLine("Hafta içi...");
+        break;
+
+    default:
+        break;
+}
+
+string dayStateOnAWeek = DateTime.Now.DayOfWeek == DayOfWeek.Saturday || DateTime.Now.DayOfWeek == DayOfWeek.Sunday ? "Hafta sonu" : "Hafta içidir";
+
--- IntroCsharp/IntroCsharp/Program.cs
+++ IntroCsharp/IntroCsharp/Program.cs
@@ -1,2 +1,75 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+/*
+ * Sayısal
+ *   -Tam sayı
+ *     
+ *   -Ondalıklı sayı
+ * 
+ * Sözel
+ *   - Tek karakter
+ *   - Çok karakter
+ * 
+ * Mantıksal
+ *  
+ * 
+ * 
+ */
+
+byte eightBit = 255;
+sbyte negativeEight = -8;
+
+short sixteenBit = 32767;
+ushort justPositive = 65535;
+
+int integerValue = 0;
+long longValue = 0;
+
+//ondalıklı
+double dPi = 3.14d;
+float fPi = 3.14f;
+decimal decPi = 31554557414569897.14591515674459744668774466777444577744598980m;
+
+
+/*Sözel
+ * -Tek karakter
+ * -Çok karakter
+ */
+
+char c = '{';
+string stringValue = "Vakıf katılım";
+
+bool isOk = true;
+
+
+
+try
+{
+    Console.WriteLine("Bir sayı girin:");
+    int number1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Bir sayı daha girin:");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    int total = number1 + number2;
+    checked
+    {
+        byte bTotal = (byte)total;
+        Console.WriteLine(bTotal);
+    }
+}
+catch (OverflowException)
+{
+
+    Console.WriteLine("Girdiğiniz sayılar toplamı byte tipine dönüşmüyor!");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Girdiğiniz veriler sayısal olmalı!");
+}
+
+
+
+
+
+
+
+
